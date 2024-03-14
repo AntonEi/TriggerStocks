c6af2466fb1e98181df029bbe2c8fa308da21862
Anton Eriksson <antonake@icloud.com>
2024-03-14 17:30:22 +0000

    wire up new database due to bugs

--- triggerlist/admin.py
+++ triggerlist/admin.py
@@ -7,11 +7,15 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(Trigger)
 class TriggerAdmin(SummernoteModelAdmin):
 
-    list_display = ('stock_name', 'slug', 'trigger_date')
+    list_display = ('stock_name', 'slug', 'trigger_date', 'created_on')
     search_fields = ['stock_name']
     list_filter = ('trigger_date',)
     prepopulated_fields = {'slug': ('stock_name',)}
     summernote_fields = ('summary',)
 
+@admin.register(Comment)
+class CommentAdmin(SummernoteModelAdmin):
 
-admin.site.register(Comment)
+    list_display = ('trigger', 'created_on')
+
+
