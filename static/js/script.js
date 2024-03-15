0f1fe0e03ca4bf9356966fed5dabe08cd480b534
Anton Eriksson <antonake@icloud.com>
2024-03-15 12:23:16 +0000

    Add navbar content and styling

---
+++ static/js/script.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems);
+
+    var elems2 = document.querySelectorAll('.sidenav-trigger');
+    var instances2 = M.Sidenav.init(elems2);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, {
+        edge: 'right' // Align mobile sidenav to the right
+    });
+
+    var elems2 = document.querySelectorAll('.sidenav-trigger');
+    var instances2 = M.Sidenav.init(elems2);
+});
