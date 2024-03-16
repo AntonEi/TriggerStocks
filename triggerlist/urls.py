448f0f6a6bb500b904f082766d1f608cda1ab44e
Anton Eriksson <antonake@icloud.com>
2024-03-16 17:08:16 +0000

    add allauth to the project

--- triggerlist/urls.py
+++ triggerlist/urls.py
@@ -1,7 +1,8 @@
 from . import views
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('', views.TriggerList.as_view(), name='home'),
     path('search', views.TriggerSearch.as_view(), name='search'),
+    path("accounts/", include("allauth.urls")),
 ]
